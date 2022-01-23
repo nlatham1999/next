6e1725c011eb59a5084a3ed351136069f3bf6992
Nick Latham <nlatham@zagmail.gonzaga.edu>
2022-01-23 09:02:25 -0800

    tutorial

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+import { remark } from 'remark'
+import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
@@ -24,6 +26,7 @@ export function getSortedPostsData() {
       ...matterResult.data
     }
   })
+
   // Sort posts by date
   return allPostsData.sort(({ date: a }, { date: b }) => {
     if (a < b) {
@@ -34,4 +37,51 @@ export function getSortedPostsData() {
       return 0
     }
   })
+}
+
+export function getAllPostIds(){
+  const fileNames = fs.readdirSync(postsDirectory)
+
+  // Returns an array that looks like this:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+  return fileNames.map(fileName => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, '')
+      }
+    }
+  })
+}
+
+export async function getPostData(id){
+  const fullPath = path.join(postsDirectory, `${id}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+  //use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents)
+
+  //use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content)
+  
+  const contentHtml = processedContent.toString()
+  
+  //combine the data with the id and contentHtml
+  return{
+    id,
+    contentHtml,
+    ...matterResult.data
+  }
 }
